9667e3d1a08c7072859730945428458f84f96cd1
yousuk3 <161000005+yousuk3@users.noreply.github.com>
2024-03-11 03:36:53 +0900

    cooviewer.rb  fix

--- Casks/cooviewer.rb
+++ Casks/cooviewer.rb
@@ -2,13 +2,21 @@ cask "cooviewer" do
   version "1.2b25"
   sha256 "b085124c540ce6e7cf2f8dbf1c2a0dc7ae47133d9afbc73357ac98ea168cf09e"
 
-  url "https://github.com/coo-ona/cooViewer/releases/download/#{version}/cooViewer.zip"
+  url "https://github.com/coo-ona/cooViewer/releases/download/#{version}/cooViewer.zip",
+      verified: "github.com/coo-ona/cooViewer/"
   name "cooviewer"
+  desc "simple comic viewer for Mac"
   homepage "https://coo-ona.github.io/cooViewer/"
 
   livecheck do
     url :url
-    strategy :github_latest
+    regex(/^(\d+(?:\.\d+)+[a-z]?\d*)$/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
   end
 
   app "cooViewer/cooViewer.app"
