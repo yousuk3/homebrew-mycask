612019f1afcdf09dfdaee1bb5e27bea450ed928f
yousuk3 <161000005+yousuk3@users.noreply.github.com>
2024-03-11 22:15:48 +0900

    add stackroom.rb

---
+++ Casks/stackroom.rb
@@ -0,0 +1,20 @@
+cask "stackroom" do
+  version :latest
+  sha256 :no_check
+
+  url "https://ul.h3z.jp/C1hLKjyF.7z",
+      verified: "ul.h3z.jp/"
+  name "stackroom"
+  desc "Comic file manager"
+  homepage "https://aromaticsapp.blogspot.com/"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  depends_on formula: "p7zip", macos: ">= :mojave"
+
+  app "Stackroom.app"
+
+  zap trash: "~/Library/Preferences/jp.aromatics.Stackroom.plist"
+end
